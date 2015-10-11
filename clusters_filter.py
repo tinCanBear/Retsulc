4e3f0748a46c475089ae9dfd0df60057d709360c
Albert's Bear <tiner.bear@gmail.com>
2015-10-12 00:30:15 +0300

    retlif

--- clusters_filter.py
+++ clusters_filter.py
@@ -1,153 +1,134 @@
-# __author__ = 'UriA12'
-# __author__ = 'UriA12'
-# import os
-# import re
-# import time
-# import statistics
-# import math
-# import numpy as np
-# from parse_main import main
-# import warnings
-#
-#
-# debug = False
-# np.seterr(all='ignore')
-# warnings.filterwarnings("ignore")
-# # main function, get info from "main_with_gui"
-#
-# def go(eps, min_ngbs,mini_eps, mini_min_ngbs, d_type, pth):
-#     print(eps, min_ngbs, mini_eps, mini_min_ngbs, d_type, pth)
-#     data_type = d_type # "2d", etc.
-#     epsilon = eps
-#     minimum_neighbors = min_ngbs
-#     mini_epsilon = mini_eps
-#     mini_minimum_neighbors = mini_min_ngbs
-#
-#     main_folder = pth    # where all the sub_folders are at #should be session folder
-#     directories = []
-#
-#
-#
-#
-#
-#             session_data = open(os.path.normcase(os.path.join(new_directory, session_name + "_final_summary.csv")), "w")
-#             session_data_pre = open(os.path.normcase(os.path.join(new_directory, session_name + "_pre_summary.csv")), "w")
-#             session_data_all = open(os.path.normcase(os.path.join(new_directory, session_name + "_all_summary.csv")), "w")
-#             done_file = open(os.path.normcase(os.path.join(new_directory, "done.txt")), "w")
-#             csv_titles = "test#,total_number_of_points,total_number_of_red_points,total_number_of_green_points,total_number_of_clustered_points,\
-#             total_number_of_unclustered_points,total_number_of_clustered_red_points,total_number_of_clustered_green_points,\
-#             relative_clustered_points,relative_unclustered_points,relative_red_clustered_points,relative_green_clustered_points,\
-#             #clusters,#red clusters,#green clusters,avg green in red clusters,std green in red clusters,avg red in green clusters,\
-#             std red in green clusters,avg red sphericity,std red sphericity,avg green sphericity,std green sphericity,\
-#             avg red Xangl,std red Xangl,avg red Yangl,std red Yangl,avg green Xangl,std green Xangl,avg green Yangl,\
-#             std green Yangl,avg red size,std red size,avg green size,std green size,sample density,avg red density,std red density,\
-#             avg green density,std green density,red median size,green median size,\
-#             avg red pts size,std red pts size,avg green pts size,std green pts size,colocalization %green in red,colocalization %red in green,test name\n"
-#
-#             session_data.write(csv_titles)
-#             session_data_pre.write(csv_titles)
-#             session_data_all.write(csv_titles)
-#             done_file.write("This folder is done with.\n")
-#             done_file.close()
-#         # Filtered files:
-#         if len(green_filess) > 0:
-#             for green_name in green_filess:
-#
-#
-#
-#
-# def get_name(file_name, cntr):
-#     """
-#     :param file_name: the path of the file containing the name of the test
-#     :param cntr:  default return if not successful
-#     :return: the name of the test clean of unrelevant info and file path
-#     """
-#     pre = file_name.split('/')[-1]
-#     pre = pre.split('\\')[-1]
-#     name = str(cntr)
-#     if pre is not None:
-#         ind_bott = pre.rfind('_')
-#         pre = pre[:ind_bott] if ind_bott != -1 else pre
-#         name = pre
-#     return name
-#
-# def get_name2(file_name, cntr):
-#     """
-#     :param file_name: the path of the file containing the name of the test
-#     :param cntr:  default return if not successful
-#     :return: the name of the test clean of unrelevant info and file path
-#     """
-#     pre = file_name.split('/')[-1]
-#     pre = pre.split('\\')[-1]
-#     name = str(cntr)
-#     if pre is not None:
-#         ind_bott = pre.rfind('_')
-#         pre = pre[:ind_bott] if ind_bott != -1 else pre
-#         if pre.rfind("green") != -1:
-#             ind_bott = pre.rfind('_')
-#             pre = pre[:ind_bott] if ind_bott != -1 else pre
-#         name = pre
-#     return name
-#
-# def get_name_super(folder_path):
-#     pre = folder_path.split('/')[-1]
-#     pre = pre.split('\\')[-1]
-#     return pre
-#
-# def filter_it(color, points, red_points, green_points, density, coloc, f_type, path):
-#     all_files = [];
-#     files_type = r".*?clusters_.*?\.csv" if f_type == "all" else r".*?clusters_{}?\.csv".format(f_type)
-#     for root, dirs, files in os.walk(path, topdown=True):
-#         for file in files:
-#             if re.findall(files_type, file, re.DOTALL):  # changed from 'r".*?green.*?.csv"'
-#     directories.append(main_folder)
-#     print(directories)
-#     for directory in directories:
-#         print(directory)
-#         print(os.listdir(directory))
-#         filessss = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-#         print(filessss)
-#         files = [os.path.join(directory, f) for f in filessss]
-#         if len(files) == 0:
-#             continue
-#         session_name = get_name_super(directory)
-#         particles_filess = []
-#         cntr = 0
-#         green_filess = []
-#         red_filess = []
-#         raw_green_filess = []
-#         raw_red_filess = []
-#         old_filess = []
-#
-#         nrm_cntr = 0
-#         raw_cntr = 0
-#         prtcls_cntr = 0
-#         old_cntr = 0
-#         for name in files:
-#             if re.findall(r".*?done\.txt", name, re.DOTALL):
-#                 print("Done file found!")
-#                 break
-#                 green_filess.append(os.path.join(root, name))
-#                 nrm_cntr += 1
-#                 print("normal counter: {}".format(nrm_cntr))
-#             if re.findall(r".*?red\.csv", name, re.DOTALL):  # changed from 'r".*?red.*?.csv"'
-#                 red_filess.append(os.path.join(root, name))
-#             if re.findall(r".*?green_r[ao]w\.csv", name, re.DOTALL):
-#                 raw_green_filess.append(os.path.join(root, name))
-#                 raw_cntr += 1
-#                 print("raw counter: {}".format(raw_cntr))
-#             if re.findall(r".*?red_r[ao]w\.csv", name, re.DOTALL):
-#                 raw_red_filess.append(os.path.join(root, name))
-#             # if re.findall(r".*?regions.*?\.txt", name, re.DOTALL):
-#             #     old_filess.append(os.path.join(root, name))
-#             # # if re.findall(r"particles\.csv", name):
-#             #     if not os.path.join(root, name) in final_particles_files: # NOTICE!!! NOT SUPPORTED YET!
-#             #         particles_filess.append(os.path.join(root, name))
-#             #         prtcls_cntr += 1
-#             #         print("particles counter: {}".format(prtcls_cntr))
-#
-#         if (len(green_filess) + len(raw_green_filess)) > 0:
-#             new_directory = directory + "/test_{}_eps{}_min{}_{}".format(time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime()), epsilon, minimum_neighbors, d_type)
-#             # make output folder and open files
-#             if not os.path.exists(new_directory):
-#                 os.mkdir(new_directory)
+__author__ = 'UriA12'
+import os
+import re
+import time
+import csv
+from parse_super_detect import get_res
+DEBUG = False
+
+
+
+def filter_it(color_a, points, red_points, green_points, density, coloc_a, size, files_path, dest_path):
+    MAX = 1000000.
+    MIN = -1.
+
+    color = color_a
+    coloc = coloc_a
+    if DEBUG: print(points)
+    points_lst = points.split(";")
+    if DEBUG: print(points_lst)
+
+    points_min = MIN if points_lst[0] == 'MIN' else float(points_lst[0])
+    points_max = MAX if points_lst[1] == "MAX" else float(points_lst[1])
+    if DEBUG: print(red_points)
+
+    red_points_lst = red_points.split(";")
+    red_points_min = MIN if red_points_lst[0] == "MIN" else float(red_points_lst[0])
+    red_points_max = MAX if red_points_lst[1] == "MAX" else float(red_points_lst[1])
+
+    if DEBUG: print(green_points)
+    green_points_lst = green_points.split(";")
+    green_points_min = MIN if green_points_lst[0] == "MIN" else float(green_points_lst[0])
+    green_points_max = MAX if green_points_lst[1] == "MAX" else float(green_points_lst[1])
+
+    if DEBUG: print(density)
+    density_lst = density.split(";")
+    density_min = MIN if density_lst[0] == "MIN" else float(density_lst[0])
+    density_max = MAX if density_lst[1] == "MAX" else float(density_lst[1])
+    if DEBUG: print(size)
+
+    size_lst = size.split(";")
+    size_min = MIN if size_lst[0] == "MIN" else float(size_lst[0])
+    size_max = MAX if size_lst[1] == "MAX" else float(size_lst[1])
+    #
+    # points_min_f = float(points_min)
+    # points_max_f = float(points_max)
+    #
+    # red_points_min_f = float(red_points_min)
+    # red_points_max_f = float(red_points_min)
+    #
+    # green_points_min_f = float(green_points_min)
+    # green_points_max_f = float(green_points_min)
+    #
+    # density_min_f = float(density_min)
+    # density_max_f = float(density_min)
+    #
+    # size_min_f = float(size_min)
+    # size_max_f = float(size_max)
+
+    red_list_dicts = []
+    green_list_dicts = []
+    red_list = []
+    green_list = []
+
+    for a_file in files_path:
+        if not re.findall(r".*?\.csv", a_file, re.DOTALL):
+            return 1
+    if dest_path == "": return 2
+    for a_file in files_path:
+        with open(a_file) as csvfile:
+            if DEBUG: print("opened")
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if DEBUG: print(row)
+                if color != "both":
+                    if row['color'] != color: continue
+                if float(row['#points']) > points_max or float(row['#points']) < points_min: continue
+                if float(row['#red points']) > red_points_max or float(row['#red points']) < red_points_min: continue
+                if float(row['#green points']) > green_points_max or float(row['#green points']) < green_points_min: continue
+                if float(row['density']) > density_max or float(row['density']) < density_min: continue
+                if coloc != "all":
+                    if row['colocalized'] == '0' and coloc == 'yes': continue
+                    if row['colocalized'] == '1' and coloc == 'no': continue
+                if float(row['size']) > size_max or float(row['size']) < size_min: continue
+                if color == "green" and row['color'] == "green":  green_list_dicts.append(row)
+                else: red_list_dicts.append(row)
+
+    for dic in red_list_dicts:
+        this_color = dic['color']
+        line = dic['color'] + ","+\
+               dic['#points'] +","+\
+               dic['#red points'] +","+\
+               dic['#green points'] +","+\
+               dic['sphere score'] +","+\
+               dic['angle_x'] +","+\
+               dic['angle_y'] +","+\
+               dic['size'] +","+\
+               dic['density'] +","+\
+               dic['colocalized'] + "\n"
+        if this_color == "green": green_list.append(line)
+        else: red_list.append(line)
+
+    b_list = ["NaN" for i in range(12)]
+    filters = "points@{}:{}::red_points@{}:{}::green_points@{}:{}::density@{}:{}::size@{}:{}::color@{}::colocalization@{}::".format(points_min,\
+                                                                            points_max,\
+                                                                            red_points_min,\
+                                                                            red_points_max,\
+                                                                            green_points_min,\
+                                                                            green_points_max,\
+                                                                            density_min,\
+                                                                            density_max,\
+                                                                            size_min,\
+                                                                            size_max,\
+                                                                            color,\
+                                                                            coloc)
+    avgd_line = get_res(red_list, green_list, 0, filters, b_list)
+
+    out_file = open(os.path.normcase(os.path.join(dest_path, "filtered_summary_{}.csv".format(time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())))), "w")
+    csv_titles = "test#,total_number_of_points,total_number_of_red_points,total_number_of_green_points,total_number_of_clustered_points,\
+    total_number_of_unclustered_points,total_number_of_clustered_red_points,total_number_of_clustered_green_points,\
+    relative_clustered_points,relative_unclustered_points,relative_red_clustered_points,relative_green_clustered_points,\
+    #clusters,#red clusters,#green clusters,avg green in red clusters,std green in red clusters,avg red in green clusters,\
+    std red in green clusters,avg red sphericity,std red sphericity,avg green sphericity,std green sphericity,\
+    avg red Xangl,std red Xangl,avg red Yangl,std red Yangl,avg green Xangl,std green Xangl,avg green Yangl,\
+    std green Yangl,avg red size,std red size,avg green size,std green size,sample density,avg red density,std red density,\
+    avg green density,std green density,red median size,green median size,\
+    avg red pts size,std red pts size,avg green pts size,std green pts size,colocalization %green in red,colocalization %red in green,test name\n"
+    out_file.write(csv_titles)
+    out_file.write(avgd_line)
+    out_file.close()
+    return 0
+
+    if DEBUG: print("done")
+
+
--- filter_gui.py
+++ filter_gui.py
@@ -5,27 +5,37 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
-#from clusters_filter import filter_it
+from clusters_filter import filter_it
 pic_path = "fireworks.jpg"
 
 
-fields = ('color', '#points', '#red points', '#green points', 'density', 'colocalized', 'files type', 'destination')
+fields = ('color', '#points', '#red points', '#green points', 'density', 'colocalized', 'size', 'destination')
 
-def go(entries):
+def go(entries, listbox):
    color = (entries['color'].get())
    points = (entries['#points'].get())
    red_points = (entries['#red points'].get())
    green_points = (entries['#green points'].get())
    density = (entries['density'].get())
    coloc = (entries['colocalized'].get())
-   f_type = (entries['color'].get())
-   path = "" #(entries['path'].get())
+   size = (entries['size'].get())
+   files_path = listbox.get(0, END) # a list of all the files
    dest_path = (entries['destination'].get())
-   if path == "":
-         messagebox.showinfo("Give us something to work with!", "The little people who work in this program are lost without" +
+   print(files_path)
+   if len(files_path) == 0:
+        messagebox.showinfo("Give us something to work with!", "The little people who work in this program are lost without" +
                             " some proper file paths.\nPlease help them find some files. (tip: Add Files)")
- #  filter_it(color, points, red_points, green_points, density, coloc, f_type, path, dest_path)
-   messagebox.showinfo("Work here is DONE!", "If you wish choose another folder.")
+   result = filter_it(color, points, red_points, green_points, density, coloc, size, files_path, dest_path)
+   if result == 0:
+      listbox.delete(0, END)
+      messagebox.showinfo("Work here is DONE!",\
+          "If you wish choose another folder.")
+   elif result == 1:
+      messagebox.showinfo("You tried to trick us!",\
+          "One or more of the files you selected isn't a \".csv\" file.\nChoose again (more carefully).\nThanks,\n\tThe little people.")
+   elif result == 2:
+      messagebox.showinfo("Nowhere to put it!",\
+          "You didn't provide a destination for the little people's work.\nThey won't work for nothing! (tip: add a destination folder)")
 
 
 def add_files(entries):
@@ -33,9 +43,11 @@ def add_files(entries):
     for name in path:
        listbox.insert(END, name)
 
+
 def choose_dest(entries):
     path = filedialog.askdirectory()
     entries['destination'].insert(0,path)
+
 
 def makeform(root, fields):
    entries = {}
@@ -48,18 +60,13 @@ def makeform(root, fields):
          ent = ttk.Combobox(row)
          ent['values'] = ('red', 'green', 'both')
          ent.insert(0, 'both')
-      if cntr == 6:
-         ent = ttk.Combobox(row)
-         ent['values'] = ('pre', 'final', 'all')
-         ent.insert(0, 'all')
       if cntr == 5:
          ent = ttk.Combobox(row)
          ent['values'] = ('yes', 'no', 'all')
          ent.insert(0, 'all')
       else:
-         if cntr != 7 and cntr != 0 and cntr != 5 and cntr != 6:
+         if cntr != 0 and cntr != 5 and cntr != 7:
             ent.insert(0, "MIN;MAX")
-
       row.pack(side=TOP, fill=Y, padx=5, pady=5)
       lab.pack(side=LEFT)
       ent.pack(side=RIGHT, fill=X)
@@ -76,6 +83,8 @@ if __name__ == '__main__':
 
    right_panel = Frame(root)
    left_panel = Frame(root)
+   row_of_buttons = Frame(right_panel)
+
 #left panel
    lab = Label(left_panel, width=40, text="Set the desired filters", anchor='w', justify=LEFT)
    lab.pack(side=TOP)
@@ -92,15 +101,16 @@ if __name__ == '__main__':
 
   # low_panel = Frame(lef)
 # buttons
-   b1 = Button(right_panel, text='Add File', command=(lambda e=ents: add_files(e)))
-   b1 = Button(right_panel, text='Add File', command=(lambda e=ents: add_files(e)))
-
-   b2 = Button(left_panel, text='GO!', command=(lambda e=ents: go(e)))
-   b1.pack(side=BOTTOM, padx=5, pady=5)
-
+   b_add = Button(row_of_buttons, text='Add File', command=(lambda e=ents: add_files(e)))
+   b_delete = Button(row_of_buttons, text="Remove Selected",command=lambda lb=listbox: lb.delete(ANCHOR))
+   b_add.pack(side=RIGHT, padx=5, pady=5)
+   b_delete.pack(side=LEFT, padx=5, pady=5)
+   row_of_buttons.pack(side=BOTTOM, padx=5, pady=5)
    left_panel.pack(side = LEFT)
    right_panel.pack(side = RIGHT, expand = "yes")
-   b2.pack(side=BOTTOM, padx=5, pady=5)
 
-   root.iconbitmap(r'big_flower.ico') # the icon
+   b_go = Button(left_panel, text='GO!', command=(lambda e=ents: go(e, listbox)))
+   b_go.pack(side=BOTTOM, padx=5, pady=5)
+
+   root.iconbitmap(r'big_flower2.ico') # the icon
    root.mainloop()
